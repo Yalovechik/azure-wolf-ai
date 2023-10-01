d13d76e46eb13f51a53bf5c2ab696a72e0b002b2
Анастасия Поданенко <anastasiapodanenko@MacBook-Air-Anastasia.local>
2023-10-01 10:58:59 +0300

    modify

--- .github/workflows/src/terraform/versions.tf
+++ .github/workflows/src/terraform/versions.tf
@@ -1,36 +1,16 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#         source = "hashicorp/azurerm"
-#         version = ">=3.69.0"
-#     }
-#     backend "azurerm" {
-#     resource_group_name   = "rg-terraform-tfstate"
-#     storage_account_name  = "devtfstate13148"
-#     container_name        = "tfstate"
-#     key                   = "terraform.tfstate"
-#   }
-#   }
-# }
-
-# provider "azurerm" {
-#   features {
-#     key_vault {
-#       purge_soft_delete_on_destroy = true
-#       recover_soft_deleted_key_vaults = true
-#     }
-#   }
-# }
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.69.0"
+        source = "hashicorp/azurerm"
+        version = ">=3.69.0"
     }
+    backend "azurerm" {
+    resource_group_name   = "rg-terraform-tfstate"
+    storage_account_name  = "devtfstate13148"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
   }
-
-  backend "azurerm" {}
- 
+  }
 }
 
 provider "azurerm" {
@@ -40,4 +20,24 @@ provider "azurerm" {
       recover_soft_deleted_key_vaults = true
     }
   }
+}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">=3.69.0"
+#     }
+#   }
+
+#   backend "azurerm" {}
+ 
+# }
+
+# provider "azurerm" {
+#   features {
+#     key_vault {
+#       purge_soft_delete_on_destroy = true
+#       recover_soft_deleted_key_vaults = true
+#     }
+#   }
 }
