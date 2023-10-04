b73666d6022d51d4b485a0ecf9daa8d37c860560
Анастасия Поданенко <anastasiapodanenko@MacBook-Air-Anastasia.local>
2023-10-04 16:37:20 +0300

    add

--- .github/workflows/src/terraform/main.tf
+++ .github/workflows/src/terraform/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_storage_blob" "blob_scraping" {
 }
 
 resource "azurerm_storage_table" "table_scraping" {
-  name                 = "scrapingtable-${var.prefix}-${var.environment}"
+  name                 = "scrapingtableAI"
   storage_account_name = azurerm_storage_account.storage.name
 }
 
